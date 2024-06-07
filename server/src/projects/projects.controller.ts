cd88a18606dd6af648389d3ba464e36ae53cd494
oleh-chadash <4talks@gmail.com>
2024-06-07 18:43:16 +0300

    add votecounts

--- server/src/projects/projects.controller.ts
+++ server/src/projects/projects.controller.ts
@@ -42,3 +42,42 @@ export const getProjectVotes = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const getProjectByCity = async (req: Request, res: Response) => {
+  try {
+    const { cityId } = req.params;
+    if (!cityId) {
+      return res.status(400).json({ message: "Invalid cityId" });
+    }
+    const project = await projectService.getProjectByCity(cityId);
+    res.status(200).json(project);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postVoteCounts = async (req: Request, res: Response) => {
+  try {
+    const { projectId, questionText, optionText } = req.body;
+    const project = await projectService.postVoteCounts({
+      projectId,
+      questionText,
+      optionText,
+    });
+    if (!project.project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    if (!project.questionFound) {
+      return res.status(404).json({ message: "Question not found" });
+    }
+
+    if (!project.optionFound) {
+      return res.status(404).json({ message: "Option not found" });
+    }
+
+    await project.project.save();
+    res.status(200).json({ message: "Vote counted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Vote has not been updated" });
+  }
+};
