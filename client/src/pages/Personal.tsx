9a16f15d6f37951ea1cb0c707ab19626b40674cf
oleh-chadash <4talks@gmail.com>
2024-06-17 12:58:07 +0300

    sent message

--- client/src/pages/Personal.tsx
+++ client/src/pages/Personal.tsx
@@ -5,6 +5,7 @@ const PersonalPage = () => {
         <>
           <div className="container mt-4 text-sm min-h-screen">
             <h1 className="sr-only">Personal Page</h1>
+            <UserPreview />
           </div>
         </>
       );
